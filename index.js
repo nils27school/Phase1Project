2614f217d6af320645bcf288c7fd8e8dd5ca491e
nilssvensson <nils27school@gmail.com>
2022-09-14 11:59:13 -0400

    wed

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-fetch("http://localhost:3000/Nemisis")
-.then(res => console.log(res))
+// fetch("http://localhost:3000/Nemisis")
+// .then(res => console.log(res))
 
-console.log(NemisisData)
+// console.log(NemisisData)
+
+//DOM render functions
+function renderOneCharacter(character) {
+    //build character
+    let card = document.createElement('li')
+    card.className = 'card'
+    card.innerHTML = `
+<img src="${character.imageUrl}">
+<div class="content">
+    <h4>${character.name}</h4>
+    <h5>${character.game}</h5
+    <p>
+    $<span class="vote-count">${character.votes}</span> Votes
+    <p>
+    <p>${character.description}</p>
+    </div>
+    <div class="buttons">
+    <button> Vote </button>
+    </div>
+`
+    // console.log(card)
+    //add character card to DOM
+    document.querySelector('#character-list').appendChild(card)
+}
+
+// initial render
+//get data and render our character to the DOM
+function initialize() {
+    character.forEach(animal => renderOneNemisis(character))
+}
+initialize()
