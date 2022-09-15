e1a5dd8e9d5aa2a2ac36972cc8649518904410ba
June Biadog <jb.biadog@gmail.com>
2022-09-14 21:04:35 -0400

    update template

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-fetch("www.thecocktaildb.com/api/json/v1/1/random.php")
-.then((response) => response.json())
-.then((data) => console.log(data));
+function renderOneCharacter(character) {
+    //build character
+    let card = document.createElement('li')
+    card.className = 'card'
+    card.innerHTML = `
+<img src="${character.imageUrl}">
+<div class="content">
+    <h4>${character.name}</h4>
+    <h5>${character.game}</h5
+    <p>
+    $<span class="vote-count">${character.votes}</span> Votes
+    <p>
+    <p>${character.description}</p>
+    </div>
+    <div class="buttons">
+    <button> Vote </button>
+    </div>
+`
+    // console.log(card)
+    //add character card to DOM
+    document.querySelector('#character-list').appendChild(card)
+}
+
+function getAllCharacters(){
+    fetch('http://localhost:3000/character')
+        .then(res => res.json())
+        .then(character.data => character.forEach(character => renderOneCharacter(character))
+        console.log('before fetch returns')
+}
+// initial render
+//get data and render our character to the DOM
+function initialize(){
+    getAllCharacters()
+    console.log('after get all characters')
+    // character.forEach(character => renderOneCharacter(character))
+}
+
+initialize()
